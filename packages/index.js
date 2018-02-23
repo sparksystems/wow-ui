f0c2c6d6e3794fa69938fc6c4c1f7b2d25d35d93
Tony Lin <tonylinx@qq.com>
2018-02-23 23:40:09 +0800

    updated v0.0.4
    2018-02-23

--- packages/index.js
+++ packages/index.js
@@ -1,26 +1,20 @@
 /**
  * @author tonylinx
- * Date: 18/02/13
+ * Date: 18/02/14
  */
 import WAlert from './alert'
-import WButton from './button';
+import WButton from './button'
 import WCol from './col'
 import WRow from './row'
 import WTag from './tag'
 
-const components = [
-  WButton,
-  WRow,
-  WCol,
-  WTag,
-  WAlert
-]
+const components = [WButton, WRow, WCol, WTag, WAlert]
 
 const install = function(Vue) {
   if (install.installed) return
   components.map(component => Vue.component(component.name, component))
-  MetaInfo.install(Vue)
-  Vue.prototype.$loading = WLoadingBar
+  // MetaInfo.install(Vue)
+  // Vue.prototype.$loading = WLoadingBar
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
@@ -28,14 +22,10 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 export default {
-  install,
+  // install,
   WButton,
   WRow,
   WCol,
   WTag,
-  WShowMore,
-  WLimitTextArea,
-  MetaInfo,
-  WAlert,
-  WLoadingBar
+  WAlert
 }
